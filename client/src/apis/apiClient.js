7f3bca924dfe7e5004d1394769e66b38d319b62a
mahatech20 <maharachan20@gmail.com>
2024-12-13 19:01:41 -0600

    updated the appointment fetching logic

--- client/src/apis/apiClient.js
+++ client/src/apis/apiClient.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const baseURL =
   process.env.NODE_ENV === "production"
     ? "https://miraculousmvp.com/api/v1" // Production URL
-    : "http://localhost:3000/api/v1"; // Development URL
+    : "http://localhost:3000/api/v1";   // Development URL
 
 const apiClient = axios.create({
   baseURL,
--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -2,9 +2,10 @@ const mysql = require("mysql2");
 
 const db = mysql.createConnection({
   user: process.env.DB_USER || "root",
- password: process.env.DB_PASSWORD || "7878",
- host: process.env.DB_HOST || "127.0.0.1",
- port: process.env.DB_PORT || 3306,
- database: process.env.DB_NAME || "db",
-});
+  password: process.env.DB_PASSWORD || "7878",
+  host: process.env.DB_HOST || "127.0.0.1",
+  port: process.env.DB_PORT || 3306,
+  database: process.env.DB_NAME || "db",
+}).promise(); // Use the promise-based API
 
+module.exports = db;
