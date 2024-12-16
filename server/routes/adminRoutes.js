dba7e2b4a849a97b0c026800a952acb09ad241ca
mahatech20 <maharachan20@gmail.com>
2024-12-16 10:44:25 -0600

    remove the server

--- server/routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { login } = require("../controllers/adminController");
-
-const router = express.Router();
-
-// Admin Login Route
-router.post("/login", login);
-
-module.exports = router;
